40028e23fdf2fe19bda4d2a2ca33f261b0c0284a
AllenYLJiang <AllenYLJiang@outlook.com>
2024-03-20 10:08:08 +0800

    Update vqa_eval.py

--- lavis/common/vqa_tools/vqa_eval.py
+++ lavis/common/vqa_tools/vqa_eval.py
@@ -1,16 +1,5 @@
-"""
- Copyright (c) 2022, salesforce.com, inc.
- All rights reserved.
- SPDX-License-Identifier: BSD-3-Clause
- For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-"""
-
 # coding=utf-8
 
-__author__ = "aagrawal"
-
-# This code is based on the code written by Tsung-Yi Lin for MSCOCO Python API available at the following link:
-# (https://github.com/tylin/coco-caption/blob/master/pycocoevalcap/eval.py).
 import sys
 import re
 
